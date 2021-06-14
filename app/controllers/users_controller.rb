a6adc5e88147f747f15c773340dc2f84884189d2
TRDuc <trduc@ubuntu.ubuntu-domain>
2021-06-14 14:18:55 +0700

    Chapter 7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t("welcome")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
