fe69c059544b01009906029a09b67a04acc69742
TRDuc <trduc@ubuntu.ubuntu-domain>
2021-06-15 14:43:29 +0700

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root "static_pages#home"
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
