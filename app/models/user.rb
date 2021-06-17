ee00f256a1c475f30bc3f30d6dd1368bff1c3452
TRDuc <trduc@ubuntu.ubuntu-domain>
2021-06-17 10:48:18 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.user.valid_email_regex
+  attr_accessor :remember_token
   has_secure_password
   validates :name, presence: true, length: { maximum: Settings.user.username_length }
   validates :email, presence: true, length: { maximum: Settings.user.email_username_length },
@@ -13,5 +14,18 @@ class User < ApplicationRecord
       BCrypt::Engine.cost
     end
     BCrypt::Password.create string, cost: cost
+  end
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+  def forget
+    update_column :remember_digest, nil
   end
 end
