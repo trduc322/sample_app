91505de95f5ac1c87b889160c02a4870009b2deb
TRDuc <trduc@ubuntu.ubuntu-domain>
2021-06-22 17:20:39 +0700

    Chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t "acc_not_activated"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "invalid_email_password_combination"
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.user.valid_email_regex
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   has_secure_password
   validates :name, presence: true, length: { maximum: Settings.user.username_length }
   validates :email, presence: true, length: { maximum: Settings.user.email_username_length },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: Settings.user.password_min_length }, allow_nil: true
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
   def User.digest string
     cost = if ActiveModel::SecurePassword.min_cost
       BCrypt::Engine::MIN_COST
@@ -22,11 +23,27 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_column :remember_digest, User.digest(remember_token)
   end
-  def authenticated? remember_token
-    return false unless remember_digest
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+    BCrypt::Password.new(digest).is_password? token
   end
   def forget
     update_column :remember_digest, nil
+  end
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  def downcase_email
+    self.email.downcase!
+  end
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
